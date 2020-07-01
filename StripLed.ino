5b001c4aab0a02a5dd15a7be37a8bb9120e6016f
aminearz <67560148+aminearz@users.noreply.github.com>
2020-07-01 14:45:02 +0200

    Add files via upload

---
+++ StripLed.ino
@@ -0,0 +1,39 @@
+#include "Arduino.h"
+#include <EEPROM.h>
+
+// the number of the LED pin
+const int ledPin = 16;  // 16 corresponds to GPIO16
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
+}
+ 
+void loop(){
+  
+  delay(2500);
+  digitalWrite(ledChannel,HIGH);
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(20);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(20);
+  }
+delay(2500);
+digitalWrite(ledChannel,LOW);
+}
